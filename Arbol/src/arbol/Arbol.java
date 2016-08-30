b45f8a6bcc8c6f509c7285f63f46528adf59a3e0
Juan Pizarro <jottapizarro@gmail.com>
2016-08-30 18:21:40 -0400

    funciones para recorrer en profundidad: preorden, postorden, inorden.

--- Arbol/src/arbol/Arbol.java
+++ Arbol/src/arbol/Arbol.java
@@ -28,4 +28,55 @@ public class Arbol {
         else
         raiz.insertar(valorInsertar); // llama al metodo insertar
     }
+    
+    public synchronized void recorridoPreorden()
+    {
+        ayudantePreorden(raiz);
+    }
+//metodo recursivo para recorrido en preorden
+
+    private void ayudantePreorden(NodoArbol nodo)
+    {
+        if (nodo == null)
+        return;
+
+        System.out.print(nodo.datos + " "); // mostrar datos del nodo
+        ayudantePreorden(nodo.nodoizquierdo); //recorre subarbol izquierdo
+        ayudantePreorden(nodo.nododerecho); //recorre subarbol derecho
+    }
+    
+    public synchronized void recorridoInorden()
+    {
+        ayudanteInorden(raiz);
+    }
+
+    // metodo recursivo para recorrido inorden
+
+    private void ayudanteInorden(NodoArbol nodo)
+    {
+        if (nodo == null)
+        return;
+
+        ayudanteInorden(nodo.nodoizquierdo);
+        System.out.print(nodo.datos + " ");
+        ayudanteInorden(nodo.nododerecho);
+    }
+    
+    public synchronized void recorridoPosorden()
+    {
+        ayudantePosorden(raiz);
+    }
+
+    //metodo recursivo para recorrido posorden
+
+    private void ayudantePosorden(NodoArbol nodo)
+    {
+        if (nodo == null)
+        return;
+
+        ayudantePosorden(nodo.nodoizquierdo);
+        ayudantePosorden(nodo.nododerecho);
+        System.out.print(nodo.datos + " ");
+    }
+    
 }
